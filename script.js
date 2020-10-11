a634d49ff1ff539e37e22b2127212c9e8a33ada2
pablo <ppabloggutierrez001@gmail.com>
2020-10-11 14:40:26 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Diseñadores", "Moda", "Streetwear", "Estilo", "Unicos", "customizadores"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    });
